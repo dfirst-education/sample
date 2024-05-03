0577ce77c36df43170839578ef4bde2c254848fd
dfirst-education <dfirst.education@gmail.com>
2024-05-03 23:01:13 +0900

    commit

--- _sqlserver_query/sp_configure.sql
+++ _sqlserver_query/sp_configure.sql
@@ -2,7 +2,7 @@ sp_configure 'show advanced options', 1
 go
 reconfigure with override
 go
-sp_configure 'max server memory', 4096
+sp_configure 'max server memory', 2048
 go
 reconfigure with override
 go
