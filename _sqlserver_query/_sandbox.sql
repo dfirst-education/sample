423fdc2d193117732bd2ce4757cddb87570cf1ac
dfirst-education <dfirst.education@gmail.com>
2024-05-11 16:09:40 +0900

    commit

--- _sqlserver_query/_sandbox.sql
+++ _sqlserver_query/_sandbox.sql
@@ -4,9 +4,12 @@
 
 
 
+select count(*) from df_jd 
+select count(*) from df_skill 
+
 select * from df_jd where job_id = '606346'
 select * from df_skill where job_id = '606346' order by skill_id
 
 
-
+sp_help df_jd
 
