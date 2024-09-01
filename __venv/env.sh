461c6b1941e2037e603a36aa1a0d79f8a4794480
dfirst-education <dfirst.education@gmail.com>
2024-09-01 19:41:10 +0900

    commit

--- __venv/env.sh
+++ __venv/env.sh
@@ -13,3 +13,14 @@ deactivate
 # list the virtual environments
 dir /b
 ls
+
+
+# how to display env_name in Jupyter Notebook on VSCode
+# after activating the virtual environment
+env_name\Scripts\activate
+pip install ipykernel
+python -m ipykernel install --user --name env_name --display-name "env_name"
+# restart VSCode
+jupyter kernelspec list
+# if you want to remove the env_name from Jupyter Notebook
+jupyter kernelspec uninstall env_name
